7909e347519bb3916439fbb2e7b2122a6cddab0e
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-18 22:43:37 +0200

    007

---
+++ code/sort007.js
@@ -0,0 +1,16 @@
+
+const scientists = [
+  "Albert Einstein",
+  "Isaac Newton",
+  "Charles Darwin",
+  "Galileo Galilei",
+  "Nikola Tesla",
+  "Leonardo da Vinci",
+  "Max Planck",
+  "Niels Bohr"
+];
+
+console.log(scientists.sort((a,b) => 0.5 - Math.random()));
+
+
+
