7c7af9b16d718151ce06174138c742a3a5502fc8
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-18 21:34:29 +0200

    nums

---
+++ code/sort002.js
@@ -0,0 +1,13 @@
+
+const nums = [12, 55, 7, 5, 4, 6, 66, 444, 1, 333];
+
+// sorts numbes as if they were strings
+console.log(nums.sort());
+
+// sort in ascending order
+console.log(nums.sort((a,b) => a > b ? 1 : -1));
+
+// sort in descending order
+console.log(nums.sort((a,b) => a > b ? -1 : 1));
+
+
